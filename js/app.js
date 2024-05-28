5bf61a0d8aa718c86d4140c670a69f5636286ce6
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-28 23:00:29 +0800

    feat(ui): Create hover effect

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@
 const gridContainer = document.querySelector(".container");
 for (let i = 0; i < 16; i++){
     // create rows
-    let row = document.createElement("div");
+    let columns = document.createElement("div");
+    columns.classList.toggle("columns");
     for (let j = 0; j < 16; j++){
         // create columns
         let cell = document.createElement("div");
         cell.style.padding = "1rem";
         cell.style.border = "2px solid #aaa";
-        row.appendChild(cell);
+        columns.appendChild(cell);
+        cell.classList.toggle("cell");
     }
-    gridContainer.appendChild(row);
+    gridContainer.appendChild(columns);
 }
