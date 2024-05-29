021e8673e70b4f54448e76e38a51f13b7a240760
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-29 12:18:01 +0800

    feat(ui): Add a bg clr picker

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 
 const gridContainer = document.querySelector(".grid-container");
+const body = document.querySelector("body");
+
+const bgClrPicker = document.querySelector("#bg-clr-picker");
+
+
 for (let i = 0; i < 16; i++){
     // create rows
     let columns = document.createElement("div");
@@ -14,3 +19,8 @@ for (let i = 0; i < 16; i++){
     }
     gridContainer.appendChild(columns);
 }
+
+
+bgClrPicker.addEventListener("input", (event) => {
+    body.style.backgroundColor = event.target.value;
+});
