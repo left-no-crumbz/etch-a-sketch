6890bd208bb5a8c7d83bf88fc1fa1da9425b2c87
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-30 14:27:35 +0800

    fix(perf): Use event delegation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 
 const gridContainer = document.querySelector(".grid-container");
-const body = document.querySelector("body");
-
 const bgClrPicker = document.querySelector("#bg-clr-picker");
 const penClrPicker = document.querySelector("#pen-clr-picker");
 
 let isClicked = false;
-
-
 function createGrid(width, height) {
     let fragment = document.createDocumentFragment();
     for (let i = 0; i < width; i++){
@@ -17,34 +13,25 @@ function createGrid(width, height) {
             let cell = document.createElement("div");
             row.appendChild(cell);
             cell.classList.toggle("cell");
-            cell.addEventListener("mouseover", draw);
-            
-            cell.addEventListener("mousedown", event => {
-                isClicked = true;
-            })
-            
-            cell.addEventListener("mouseup", event => {
-                if(isClicked ) {
-                    isClicked = false;
-                }
-            })
         }
         fragment.appendChild(row);
     }
     gridContainer.appendChild(fragment);
 }
 
-createGrid(100, 100);
+createGrid(16, 16);
 
-const cell = document.querySelectorAll(".cell");
 function draw(event) {
     const target = event.target;
-    
-    if (isClicked){
+    if (target.classList.contains("cell") && isClicked){
         target.style.backgroundColor = "black";
     }
 
 }
+
+gridContainer.addEventListener("mouseover", draw);
+gridContainer.addEventListener("mousedown", () => {isClicked = true;});
+gridContainer.addEventListener("mouseup", () => {isClicked = false;});
 
 bgClrPicker.addEventListener("input", (event) => {
     body.style.backgroundColor = event.target.value;
