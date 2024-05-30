4c669c3602da38c7a4d4ba134fdc42f8d3565481
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-30 13:40:38 +0800

    feat: Add drawing feature

--- js/app.js
+++ js/app.js
@@ -5,10 +5,8 @@ const body = document.querySelector("body");
 const bgClrPicker = document.querySelector("#bg-clr-picker");
 const penClrPicker = document.querySelector("#pen-clr-picker");
 
-const cell = document.querySelectorAll(".cell");
-function draw(event) {
-    console.log(`Mouse entered ${event.target}`)         
-}
+let isClicked = false;
+
 
 function createGrid(width, height) {
     for (let i = 0; i < width; i++){
@@ -25,15 +23,34 @@ function createGrid(width, height) {
             // cell.style.border = "2px solid black";
             row.appendChild(cell);
             cell.classList.toggle("cell");
+            cell.addEventListener("mouseover", draw);
+            
+            cell.addEventListener("mousedown", event => {
+                isClicked = true;
+            })
+            
+            cell.addEventListener("mouseup", event => {
+                if(isClicked ) {
+                    isClicked = false;
+                }
+            })
         }
         gridContainer.appendChild(row);
     }
 }
 
+createGrid(16, 16);
 
-gridContainer.addEventListener("mouseenter", draw);
+const cell = document.querySelectorAll(".cell");
+function draw(event) {
+    const target = event.target;
+    
+    if (isClicked){
+        target.style.backgroundColor = "black";
+    }
+
+}
+
 bgClrPicker.addEventListener("input", (event) => {
     body.style.backgroundColor = event.target.value;
 });
-
-createGrid(16, 16);
