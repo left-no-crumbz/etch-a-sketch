152f8feac0d5dd38a81c68b9071e2f1757c54f37
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-31 22:40:13 +0800

    feat: Add eraser feature

--- js/app.js
+++ js/app.js
@@ -5,12 +5,14 @@ const penClrPicker = document.querySelector("#pen-clr-picker");
 const body = document.querySelector("#body");
 const rainbowBtn = document.querySelector("#rainbow");
 const shadingBtn = document.querySelector("#shading");
+const eraseBtn = document.querySelector("#eraser");
 const sidebar = document.querySelector(".sidebar");
 
 let penClr = penClrPicker.value;
 let rainbowMode = false;
-let isClicked = false;
+let clicked = false;
 let shadingMode = false; 
+let eraseMode = false; 
 
 function createGrid(width, height) {
     let fragment = document.createDocumentFragment();
@@ -27,7 +29,7 @@ function createGrid(width, height) {
     gridContainer.appendChild(fragment);
 }
 
-createGrid(32, 32);
+createGrid(16, 16);
 
 sidebar.addEventListener("input", (event) => {
     target = event.target;
@@ -47,10 +49,14 @@ sidebar.addEventListener("click", (event) => {
             target.style.border = "0";
             shadingBtn.disabled = false;
             shadingBtn.style.cursor = "pointer";
+            eraseBtn.disabled = false;
+            eraseBtn.style.cursor = "pointer";
         } else {
             rainbowMode = true;
             shadingBtn.disabled = true;
             shadingBtn.style.cursor = "not-allowed";
+            eraseBtn.disabled = true;
+            eraseBtn.style.cursor = "not-allowed";
             target.style.border = "1px solid #7289da";
         }
     } else if (target.id === "shading") {
@@ -59,39 +65,60 @@ sidebar.addEventListener("click", (event) => {
             target.style.border = "0";
             rainbowBtn.disabled = false;
             rainbowBtn.style.cursor = "pointer";
+            eraseBtn.disabled = false;
+            eraseBtn.style.cursor = "pointer";
         } else {        
             shadingMode = true;
             rainbowBtn.disabled = true;
             rainbowBtn.style.cursor = "not-allowed";
+            eraseBtn.disabled = true;
+            eraseBtn.style.cursor = "not-allowed";
+            target.style.border = "1px solid #7289da";
+        }
+    } else if (target.id === "eraser") {
+        if(eraseMode){
+            eraseMode = false;
+            target.style.border = "0";
+            rainbowBtn.disabled = false;
+            rainbowBtn.style.cursor = "pointer";
+            shadingBtn.disabled = false;
+            shadingBtn.style.cursor = "pointer";
+        } else {
+            eraseMode = true;
+            rainbowBtn.disabled = true;
+            rainbowBtn.style.cursor = "not-allowed";
+            shadingBtn.disabled = true;
+            shadingBtn.style.cursor = "not-allowed";
             target.style.border = "1px solid #7289da";
         }
     }
 });
 
 gridContainer.addEventListener("mouseover", draw);
-gridContainer.addEventListener("mousedown", () => {isClicked = true;});
-gridContainer.addEventListener("mouseup", () => {isClicked = false;});
+gridContainer.addEventListener("mousedown", () => {clicked = true;});
+gridContainer.addEventListener("mouseup", () => {clicked = false;});
 
 
 function draw(event) {
     const target = event.target;
-    let cellIsClicked = target.classList.contains("cell") && isClicked;
-    if (cellIsClicked && !rainbowMode && !shadingMode){
+    let cellIsClicked = target.classList.contains("cell") && clicked;
+    if (cellIsClicked && !rainbowMode && !shadingMode && !eraseMode){
         target.style.backgroundColor = penClr;
     } else if(cellIsClicked && rainbowMode) {
-        const MAX = 256;
-        const MIN = 0;
-        let r = Math.floor(Math.random() * (MAX - MIN) + MIN);
-        let g = Math.floor(Math.random() * (MAX - MIN) + MIN);
-        let b = Math.floor(Math.random() * (MAX - MIN) + MIN);
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
         target.style.backgroundColor = `rgb(${r},${g},${b})`;
-    } else if (cellIsClicked && shadingMode && !rainbowMode){
+    } else if (cellIsClicked && shadingMode && !rainbowMode && !eraseMode){
         target.style.backgroundColor = penClr;
         if (target.style.opacity >= 0.1) {
             target.style.opacity = +target.style.opacity + 0.1;
         } else {
             target.style.opacity = 0.1;
         }
+    } else if (cellIsClicked && eraseMode && !rainbowMode && !shadingMode){
+        target.style.opacity = "1";
+        target.style.backgroundColor = "white";
     }
 
 }
